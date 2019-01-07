aac415caf5aa0422947524d78ec1a4c58b3513a9
Martin Giger <martin@humanoids.be>
2019-01-07 19:20:48 +0100

    also package the license

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 python3 -m pip install -U --target=. --platform=linux_armv7l --only-binary=:all: --no-compile --extra-index-url=https://www.piwheels.org/simple -r requirements.txt
-zip -r WebOfTXT.zip main.py icon.png manifest ifaddr jsonschema pyee tornado webthing zeroconf.py netifaces.cpython-35m-arm-linux-gnueabihf.so
+zip -r WebOfTXT.zip main.py icon.png manifest LICENSE ifaddr jsonschema pyee tornado webthing zeroconf.py netifaces.cpython-35m-arm-linux-gnueabihf.so
